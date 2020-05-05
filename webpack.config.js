a2f76c9ebeaa4d7d24b1e9552440f8430d9ce5f6
Annada Jena <annada.jena@rakuten.com>
2020-05-05 21:36:09 +0530

    Chnaged index.html location

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: './public/index.html',
-      filename: './index.html'
+      filename: '../index.html'
     }),
     new MiniCssExtractPlugin({
       filename: isDev ? '[name].css' : '[name].[hash].css',
