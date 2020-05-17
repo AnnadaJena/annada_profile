bf20f86a5d128281a79a229543a018040f49f90c
Annada Jena <annada.jena@rakuten.com>
2020-05-18 00:07:33 +0530

    index html fix

--- docs/src-sw.js
+++
@@ -1,4 +0,0 @@
-importScripts("precache-manifest.d26babcd2c1338ad01628cd0ab83619d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
